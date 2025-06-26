4ca6963ab70ba527c62f7c48990ceb2a5df12416
JungDongSeop <ds123911@gmail.com>
2025-06-26 15:12:10 +0900

    fix: profile_image URL api에서 nickname도 받아오도록 수정

--- src/main/java/com/pieceofcake/memberservice/member/dto/out/ReadProfileImageResponseDto.java
+++ src/main/java/com/pieceofcake/memberservice/member/dto/out/ReadProfileImageResponseDto.java
@@ -4,24 +4,35 @@ import com.pieceofcake.memberservice.member.entity.Member;
 import com.pieceofcake.memberservice.member.vo.in.ReadProfileImageRequestVo;
 import com.pieceofcake.memberservice.member.vo.out.ReadProfileImageResponseVo;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class ReadProfileImageResponseDto {
     private String profileImageUrl;
+    private String nickname;
 
     @Builder
-    public ReadProfileImageResponseDto(String profileImageUrl) {
+    public ReadProfileImageResponseDto(
+            String profileImageUrl,
+            String nickname
+    ) {
         this.profileImageUrl = profileImageUrl;
+        this.nickname = nickname;
     }
 
     public static ReadProfileImageResponseDto from(Member member) {
         return ReadProfileImageResponseDto.builder()
                 .profileImageUrl(member.getProfileImageUrl())
+                .nickname(member.getNickname())
                 .build();
     }
 
     public ReadProfileImageResponseVo toVo() {
         return ReadProfileImageResponseVo.builder()
                 .profileImageUrl(profileImageUrl)
+                .nickname(nickname)
                 .build();
     }
 }
